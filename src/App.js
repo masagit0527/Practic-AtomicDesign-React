f9c36b44e2049efb04ca4b65dbfc325de9542aa4
Suzuki Masato <82252787+masagit0527@users.noreply.github.com>
2022-07-23 06:11:59 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Router } from "./router/Router";
+import "./styles.css";
+
+const user = {
+  name: "ショウ",
+  image: "https://source.unsplash.com/vmBik4xv27s",
+  email: "test@example.com",
+  phone: "0120-000-111",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "htttps://google.com"
+};
+
+export default function App() {
+  return <Router />;
+}
